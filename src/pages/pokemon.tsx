0537276c37cdedaf9d8b23e18aaa1ab4eb3f6014
Matheus Paz <72666688+MatheusPazDev@users.noreply.github.com>
2021-01-16 19:33:28 -0300

    Correção do Link para link

--- src/pages/pokemon.tsx
+++ src/pages/pokemon.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout";
-import Link from "next/Link";
+import Link from "next/link";
+
 export default function pokemon({ pokeman }) {
   return (
     <Layout title={pokeman.name}>
